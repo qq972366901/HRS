ce68d859c547aec5fe25cd8c392976dcdd7f8e09
qq972366901 <972366901@qq.com>
2016-12-10 20:25:27 +0800

    删除了没用的方法

--- Client/src/main/java/roomUpdate/RoomUpdateController.java
+++ Client/src/main/java/roomUpdate/RoomUpdateController.java
@@ -1,11 +1,7 @@
 package roomUpdate;
 
-import java.util.ArrayList;
-
 import RoomBLServiceImpl.RoomStateChange;
 import RoomBLServiceImpl.RoomsReserved;
-import VO.OrderVO;
-import VO.RoomVO;
 
 public class RoomUpdateController{
 	/**
@@ -32,52 +28,4 @@ public class RoomUpdateController{
 		roomsReserved.updateRoomsState(roomType, roomNumber);
 	}
 	
-	/**
-     * 修改房间信息
-     * 
-     * @param in MessageInput型，界面输入的修改信息
-     * @return 返回ResultMessage枚举值中的一项
-     * @see bussinesslogic.Room
-     */
-	public ResultMessage messageupdate(MessageInput in){
-		return ResultMessage.Exist;
-	}
-	
-	
-	
-    /**
-     * 修改酒店含有的房间信息
-     * 
-     * @param in MessageInput型，界面输入的修改信息
-     * @return 返回ResultMessage枚举值中的一项
-     * @see bussinesslogic.Room
-     */
-	public ResultMessage messageadd(MessageInput  in) {
-		return ResultMessage.Exist;
-	}
-	
-	
-	/**
-     * 获得订单上的客户的个人信息和入住退房时间
-     * 
-     * @param vo OrderVO型，一个OrderVO，订单值对象
-     * @return RoomVO返回一个房间值对象
-     * @see bussinesslogic.Room
-     */
-	public RoomVO messageadd(OrderVO  vo) {
-		RoomVO rv=new RoomVO();
-		return rv;
-	}
-	
-	
-	/**
-     * 返回可入住房间
-     * 
-     * @return ArrayList<RoomVO>,一个房间值对象列表
-     * @see bussinesslogic.Room
-     */
-	public ArrayList<RoomVO> getRoom() {
-		ArrayList<RoomVO> array=new ArrayList<RoomVO>(); 
-		return array;
-	}
 }
