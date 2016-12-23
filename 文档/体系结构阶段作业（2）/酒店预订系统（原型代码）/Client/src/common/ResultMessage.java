bc35643044b777e7666528cd2cb6064be4449835
qq972366901 <972366901@qq.com>
2016-12-23 22:02:20 +0800

    m

--- 文档/体系结构阶段作业（2）/酒店预订系统（原型代码）/Client/src/common/ResultMessage.java
+++
@@ -1,5 +0,0 @@
-package common;
-
-public enum ResultMessage {
-	Exist,False,Unknown
-}
--- 文档/体系结构阶段作业（2）/酒店预订系统（原型代码）/Server/src/dataService/DataService_Driver.java
+++
@@ -1,7 +0,0 @@
-package dataService;
-
-public class DataService_Driver {
-	public void drive(DataService dataService){
-		
-	}
-}
